4b41caf1f86584415d8f54124305e01461beb37d
Serge Singizwa <singizwaserge@gmail.com>
2025-07-23 17:12:42 +0200

    first commit

---
+++ migrations/20250723144855-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { ENUM } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+         primaryKey:true,
+         type:Sequelize.UUID,
+         defaultValue:Sequelize.UUIDV4
+
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique:true
+      },
+      password: {
+        type: Sequelize.STRING
+
+      },
+      role:{
+        type:Sequelize.STRING,
+        defaultValue:'user'
+
+      },
+      gender:{
+        type:ENUM('male','female','other')
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
+config()
+const app =express();
+const port=process.env.PORT||5000;
+ app.get('/',(req,res)=>{
+    res.send("Hello Rwanda")
+ }
+);
+
+app.listen(port,()=>{
+    console.log("My App is running on Heaven")
+});
