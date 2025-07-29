0f00051c008ee3fdcab3914a88b2c6606ae2b56f
Serge Singizwa <singizwaserge@gmail.com>
2025-07-29 15:08:08 +0200

    validating user

--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import Authrouter from "./authRoutes";
+import { userRouter } from "./userRoutes";
 
 export const router = Router();
 
 router.use('/auth', Authrouter); 
+
+router.use('/api/v1',userRouter)
